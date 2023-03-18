e48ed710933905ff9212a257bfdf34822591822f
Arnab55555 <122381538+Arnab55555@users.noreply.github.com>
2023-03-18 23:18:48 +0530

    change

---
+++ email2.py
@@ -0,0 +1,29 @@
+import os
+import email
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+
+email_sender = ''
+email_password = ''
+email_reciever = ''
+
+subject = 'test mail'
+body = """
+I am trying email automation using python.
+"""
+
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To']  = email_reciever
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
