2fe70c10a3bda5808b6d39b46cc5d00e25714634
Arnab55555 <122381538+Arnab55555@users.noreply.github.com>
2023-04-07 20:13:19 +0530

    changes

--- ViewBooks.py
+++ ViewBooks.py
@@ -1,6 +1,8 @@
+import tkinter
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
+from tkinter import ttk
 import sqlite3
 
 # Add your own database name and password here to reflect in the code
@@ -32,19 +34,45 @@ def View():
     headingLabel = Label(headingFrame1, text="View Books", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
     
-    labelFrame = Frame(root,bg='black')
-    labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)
-    y = 0.25
+    #tree defining
+    tree = ttk.Treeview(root)
+    tree['show']="headings"
+
+
+    tree['columns']=("BOOK_ID","BOOK_NAME","BOOK_TYPE","BOOK_PRICE","BOOK_COUNT","BOOK_AUTHOR")
+    tree.column("BOOK_ID",width=50,minwidth=50,anchor=tkinter.CENTER)
+    tree.column("BOOK_NAME",width=100,minwidth=100,anchor=tkinter.CENTER)
+    tree.column("BOOK_TYPE",width=50,minwidth=50,anchor=tkinter.CENTER)
+    tree.column("BOOK_PRICE",width=150,minwidth=150,anchor=tkinter.CENTER)
+    tree.column("BOOK_COUNT",width=150,minwidth=150,anchor=tkinter.CENTER)
+    tree.column("BOOK_AUTHOR",width=150,minwidth=150,anchor=tkinter.CENTER)
+
+    tree.heading("BOOK_ID",    text="BOOK_ID",anchor=tkinter.CENTER)
+    tree.heading("BOOK_NAME",  text="BOOK_NAME",anchor=tkinter.CENTER)
+    tree.heading("BOOK_TYPE",  text="BOOK_TYPE",anchor=tkinter.CENTER)
+    tree.heading("BOOK_PRICE", text="BOOK_PRICE",anchor=tkinter.CENTER)
+    tree.heading("BOOK_COUNT", text="BOOK_COUNT",anchor=tkinter.CENTER)
+    tree.heading("BOOK_AUTHOR",text="BOOK_AUTHOR",anchor=tkinter.CENTER)
+
+
+
+    # labelFrame = Frame(root,bg='black')
+    # labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)
+    # y = 0.25
     
-    Label(labelFrame, text="%-10s%-70s%-10s%-10s%-10s%-20s"%('BID','Title','Type','Price','Count','Author'),bg='black',fg='white').place(relx=0.07,rely=0.1)
-    Label(labelFrame, text="-------------------------------------------------------------------------------------",bg='black',fg='white').place(relx=0.05,rely=0.2)
+    # Label(labelFrame, text="%-10s%-70s%-10s%-10s%-10s%-20s"%('BID','Title','Type','Price','Count','Author'),bg='black',fg='white').place(relx=0.07,rely=0.1)
+    # Label(labelFrame, text="-------------------------------------------------------------------------------------",bg='black',fg='white').place(relx=0.05,rely=0.2)
     getBooks = "select * from "+bookTable
     try:
         database_cursor.execute(getBooks)
         database_connection.commit()
-        for i in database_cursor:
-            Label(labelFrame, text="%-10s%-40s%-10s%-10s%-10s%-20s"%(i[0],i[1],i[2],i[3],i[4],i[5]),bg='black',fg='white').place(relx=0.07,rely=y)
-            y += 0.1
+        i = 0
+        for ro in database_cursor:
+            # Label(labelFrame, text="%-10s%-40s%-10s%-10s%-10s%-20s"%(i[0],i[1],i[2],i[3],i[4],i[5]),bg='black',fg='white').place(relx=0.07,rely=y)
+            # y += 0.1
+            tree.insert('',i, text="",values=(ro[0],ro[1],ro[2],ro[3],ro[4],ro[5]))
+            i = i+1
+        tree.pack()
     except:
         messagebox.showinfo("Failed to fetch files from database")
     
